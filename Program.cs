408026ff829d1b310cfede298274869dca5704fb
LyudmilaGit1 <118212202+LyudmilaGit1@users.noreply.github.com>
2022-12-03 12:07:42 +0000

    добавила задачу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿\\ Напишите программу, которая принимает на вход 3 числа и выдает максимальное из этих чисел.
+
+Console.Write("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if(num1>num2>num3)
+{
+Console.WriteLine("num1-максимальное число");
+}
+else
+if(num1>num2<num3)
+{
+Console.WriteLine("num3-максимальное число");
+}
+else
+if(num2>num1>num3)
+{
+Console.WriteLine("num2-максимальное число");
+}
