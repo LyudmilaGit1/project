b5228dd12a62b9b4a15207e1f0112b4d9778c7ca
LyudmilaGit1 <118212202+LyudmilaGit1@users.noreply.github.com>
2022-12-03 12:47:49 +0000

    заработало

---
+++ homework/ex2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход 3 числа и выдает максимальное из этих чисел.
+
+Console.Write("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+if(num1 > num2);
+if(num1 > num3)
+{
+Console.WriteLine("первое число максимальное");
+}
+else
+if(num3<num2) 
+{
+Console.WriteLine("второе число максимальное ");
+}
+else
+if(num3>num2)
+{
+Console.WriteLine("третье число максимальное ");
+}
