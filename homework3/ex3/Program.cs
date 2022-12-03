c6c79d7949baa150e5023f39c9b0f52d955679be
LyudmilaGit1 <118212202+LyudmilaGit1@users.noreply.github.com>
2022-12-03 19:20:10 +0000

    кубы

---
+++ homework3/ex3/Program.cs
@@ -0,0 +1,9 @@
+﻿//3. принимает (N), и выдает таблицу кубов от 1 до N
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for(int i=1;i<=N; i++)
+{
+    Console.WriteLine($"{i}*{i}*{i}={i*i*i}");
+}
