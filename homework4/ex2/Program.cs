1d9a95fde03fa14edead23b3945d79bbf2eda619
LyudmilaGit1 <118212202+LyudmilaGit1@users.noreply.github.com>
2022-12-08 15:54:45 +0000

    Хоть что-то работает

--- homework4/ex2/Program.cs
+++ homework4/ex2/Program.cs
@@ -5,3 +5,17 @@
 //82 -> 10
 
 //9012 -> 12
+
+Console.Write("Введите число: ");
+int x=Convert.ToInt32(Console.ReadLine());
+string Num =Convert.ToString(x);
+int L=Num.Length;
+int n=0;
+for(int i=0;i<L;i++)
+
+ 
+Console.WriteLine(Num[i]);
+{
+
+}
+Console.WriteLine(n);
