c09cbd7bc954f2952777061de74804a71008b0ec
LyudmilaGit1 <118212202+LyudmilaGit1@users.noreply.github.com>
2022-12-11 13:43:38 +0000

    Число в натуральной степени

---
+++ homework4/ex1/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+//3, 5 -> 243 (3⁵)
+
+//2, 4 -> 16
+//
+Console.Write("Введите число: ");
+double x= Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень числа: ");
+double y= Convert.ToInt32(Console.ReadLine());
+double res =Math.Abs(y);
+double result= Math.Pow(x,res);
+Console.WriteLine(result);
