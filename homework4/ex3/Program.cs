f371ab34286314b7521a7ea31b72ccd070690330
LyudmilaGit1 <118212202+LyudmilaGit1@users.noreply.github.com>
2022-12-11 13:58:34 +0000

    массив

--- homework4/ex3/Program.cs
+++ homework4/ex3/Program.cs
@@ -3,3 +3,20 @@
 //1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 
 //6, 1, 33 -> [6, 1, 33]
+ int [] arrRandom(int len)
+{
+    int[]a=new int[len];
+   for(int i=0;i<a.Length;i++)
+ {
+  a[i]=new Random().Next(0,1000);
+ }
+  return a;
+}
+void arrPrint(int[]a)
+{
+for(int i=0;i<a.Length;i++)
+Console.Write($"{a[i]},");
+Console.WriteLine();
+}
+int[] arr=arrRandom(8);
+arrPrint(arr);
