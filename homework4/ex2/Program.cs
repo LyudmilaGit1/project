439100cf378aef4cb6849a91bdbeb01c7f79ba61
LyudmilaGit1 <118212202+LyudmilaGit1@users.noreply.github.com>
2022-12-11 13:39:23 +0000

    Сумма цифр в числе

--- homework4/ex2/Program.cs
+++ homework4/ex2/Program.cs
@@ -9,13 +9,16 @@
 Console.Write("Введите число: ");
 int x=Convert.ToInt32(Console.ReadLine());
 string Num =Convert.ToString(x);
+
 int L=Num.Length;
+int[] M=new int[L];
+int m=x;
 int n=0;
 for(int i=0;i<L;i++)
-
- 
-Console.WriteLine(Num[i]);
 {
-
+  M[i]=m%10;
+  m/=10;
+ 
+ n=M[i]+n;
 }
 Console.WriteLine(n);
