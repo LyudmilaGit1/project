179b9fe79f9b82e47c1335c5d228ad788501a4f1
LyudmilaGit1 <118212202+LyudmilaGit1@users.noreply.github.com>
2022-12-11 12:35:05 +0000

    разница между max и min

---
+++ homework5/ex3/Program.cs
@@ -0,0 +1,38 @@
+﻿
+//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+//[3 7 22 2 78] -> 76
+int[] arL(int Len)
+{
+int[] a = new int[Len];
+for(int i=0;i<a.Length;i++)
+{
+    a[i]=new Random().Next(0,100);
+    }
+    return a;
+}  
+    void arPrint(int[] a)
+    {
+        for(int i=0;i<a.Length;i++)
+        Console.Write($"{a[i]}'");
+        Console.WriteLine();
+    }    
+    int []ar=arL(11);
+    arPrint(ar);
+{
+int N=ar.Length;    
+int max=ar[0];
+int min=ar[0];
+
+for(int i=0;i<N;i++)
+{
+   if(ar[i]>max)
+
+  max=ar[i];
+else
+if(ar[i]<min)
+
+  min=ar[i];
+}
+Console.WriteLine(max-min);
+}
