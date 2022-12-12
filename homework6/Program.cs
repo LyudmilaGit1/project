b8a2f6a12e425bcbb8a435441dee855f6a903114
LyudmilaGit1 <118212202+LyudmilaGit1@users.noreply.github.com>
2022-12-12 19:47:18 +0000

    ?

---
+++ homework6/Program.cs
@@ -0,0 +1,14 @@
+﻿//вывести первые N чисел фибоначи. первое 0, второе 1
+WriteLine("Введите число: ");
+WriteLine(string.Join(Febonacci(int.Parse(ReadLine()))));
+int[] Febonacci(int n);
+{
+    int[] count = new int[n];
+    count[0]=0;
+    count[1]=1;
+    for(int i=2;i<=count.Length-1;i++)
+    {
+        count[i]=count[i-1]+count[i-2];
+    }
+    return count;
+}
