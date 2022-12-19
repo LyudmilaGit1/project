3c56b68404397442e41ff52b9ec856d43c3b6966
LyudmilaGit1 <118212202+LyudmilaGit1@users.noreply.github.com>
2022-12-19 22:20:34 +0000

    Строка с наименьшей суммой

--- homework8/ex2/Program.cs
+++ homework8/ex2/Program.cs
@@ -9,6 +9,9 @@
 //8 4 2 4
 
 //5 2 6 7
+
+//Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
 void PrintArray(int[,] table)
 {
 for(int i=0;i<table.GetLength(0);i++)
@@ -34,30 +37,44 @@ int [,] tabl= new int[3,4];
 FillArray(tabl);
 PrintArray(tabl);
 Console.WriteLine();
-int summ=0;
-int summ1=0;
-int summ2=0;
-
-
 
 
 for(int i=0;i<tabl.GetLength(0);i++)
  {
-    for(int j=0;j<tabl.GetLength(1);j++)
+    int summ=0;
+    int min=summ;
+       for(int j=0;j<tabl.GetLength(1);j++)
       {
-        if(i==0) summ=tabl[i,j]+summ; 
-        if(i==1) summ1=tabl[i,j]+summ1; 
-        if(i==2) summ2=tabl[i,j]+summ2;
-           
-     }
-     }
-     Console.Write($"{summ},{summ1},{summ2},"); 
-     Console.WriteLine();
-     if(summ<summ1 && summ<summ2)
-     Console.Write($"первая строка{summ}");
-     if(summ1<summ && summ1<summ2)
-     Console.Write($"вторая строка{summ1}");
-     if(summ2<summ && summ2<summ1)
-     Console.Write($"третья строка{summ2}");
+       
+       summ=tabl[i,j]+summ; 
+       if(summ<min) min=summ;
+      }   
+      
+   Console.Write($"{summ},");   
+   
+   }
+int SumElem(int[,] table, int i)
+{
+  int sumLine = tabl[i,0];
+  for (int j = 1; j < tabl.GetLength(1); j++)
+  {
+    sumLine += tabl[i,j];
+  }
+  return sumLine;
+}
 
-   Console.WriteLine();
+int minSum = 0;
+int sumLine = SumElem(tabl, 0);
+for (int k = 1; k < tabl.GetLength(0); k++)
+{
+  int SumLine = SumElem(tabl, k);
+  if (sumLine > SumLine)
+  {
+    sumLine = SumLine;
+    minSum = k;
+  }
+}
+
+Console.WriteLine($"\n{minSum+1} - строкa с наименьшей суммой({sumLine})");
+
+
