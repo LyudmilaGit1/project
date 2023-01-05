6aa4917605a83cb33194beafd268e3a430637554
Lyudmila <lyudmila150983@gmail.com>
2023-01-05 13:30:36 +0300

    Сумма чисел с рекурсией я не совладала

---
+++ homework9/ex2/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+Console.Write("Введите натуральное число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Summ(int M, int N)
+		{
+			int First = M;
+			int Last = N;
+			if(M > N)
+			{
+				First = N;
+				Last = M;
+			}
+			return (Last + First)*(Last - First + 1)/2; 
+		}
+		
+		Console.WriteLine(Summ(m, n));
+
+  
